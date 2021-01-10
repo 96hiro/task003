1ff7623d290af3897e1ce1165f80fe7d587db0c7
96hiro <shinodaai@shinodaainoMacBook-Pro.local>
2021-01-10 17:40:40 +0900

    add new file

---
+++ search.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import eel
+
+### デスクトップアプリ作成課題
+def kimetsu_search(word,file_name,reslt_write):
+    # 検索対象取得
+    df=pd.read_csv(file_name)
+    source=list(df["name"])
+
+    # 検索
+    if word in source:
+        # print("『{}』はあります".format(word))
+        eel.view_log_js("{}はあります。".format(word))
+        # eel.view_log_js()
+    else:
+        # print("『{}』はありません".format(word))
+        eel.view_log_js("{}はありません" .format(word))
+        # 追加
+        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        #if add_flg=="1":
+        source.append(word)
+    
+    # CSV書き込み
+    df=pd.DataFrame(source,columns=["name"])
+    df.to_csv("./source.csv",encoding="utf_8-sig")
+    print(source)
